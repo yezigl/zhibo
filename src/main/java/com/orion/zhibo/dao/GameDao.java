4390f2f40fecb4a767899041c611b6ca7f8f7380
lidehua <lidehua@meituan.com>
2015-09-08 14:17:54 +0800

    f

--- src/main/java/com/orion/zhibo/dao/GameDao.java
+++ src/main/java/com/orion/zhibo/dao/GameDao.java
@@ -6,13 +6,11 @@ package com.orion.zhibo.dao;
 import java.util.List;
 
 import org.mongodb.morphia.Datastore;
-import org.mongodb.morphia.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.orion.mongodb.dao.AbstractEntityDao;
 import com.orion.zhibo.entity.Game;
-import com.orion.zhibo.entity.Platform;
 
 /**
  * description here
@@ -31,10 +29,9 @@ public class GameDao extends AbstractEntityDao<Game> {
         super(datastore);
     }
     
-    public List<Game> listByPlatform(Platform platform) {
-        Query<Game> query = createQuery();
-        query.field("platform").equal(platform);
-        return query.asList();
+    @Override
+    public List<Game> getAll() {
+        return this.createQuery().order("abbr").asList();
     }
 
 }
