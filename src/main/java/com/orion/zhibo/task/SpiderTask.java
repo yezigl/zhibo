a42d8a9916909b6695c62063230269e95baa90a7
lukeli <lukeli@sohu-inc.com>
2015-10-22 00:30:03 +0800

    fix

--- src/main/java/com/orion/zhibo/task/SpiderTask.java
+++ src/main/java/com/orion/zhibo/task/SpiderTask.java
@@ -27,7 +27,7 @@ public class SpiderTask {
     @Autowired
     List<Spider> spiders;
     
-    @Scheduled(cron = "0 */1 * * * ?")
+    @Scheduled(cron = "0 */10 * * * ?")
     public void run() {
         logger.info("spider run {}", spiders.size());
         for (Spider spider : spiders) {
