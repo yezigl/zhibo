988f7efcbb631896df8426d87a47ec1050e1aca3
lidehua <lidehua@meituan.com>
2015-12-08 15:24:45 +0800

    fix

--- script/deploy.sh
+++ script/deploy.sh
@@ -6,7 +6,12 @@ version=0.0.1
 jar=$module-$version-SNAPSHOT.jar
 pid=`ps -ef | grep $jar | grep -v grep | awk '{print $2}'`
 
-cd /opt/$app
+webroot=/opt/$app
+if [ ! -d $webroot ] ; then
+    sudo mkdir $webroot && sudo chown -R devel:devel $webroot
+fi
+
+cd $webroot
 
 if [ -n "$pid" ] ; then
     kill $pid
