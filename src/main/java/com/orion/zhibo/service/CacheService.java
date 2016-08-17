c5bec5e61e2a034342e60160e299b296b245fcf4
lidehua <lidehua@meituan.com>
2016-08-17 11:41:04 +0800

    remove redis

--- src/main/java/com/orion/zhibo/service/CacheService.java
+++ src/main/java/com/orion/zhibo/service/CacheService.java
@@ -3,10 +3,15 @@
  */
 package com.orion.zhibo.service;
 
-import java.util.concurrent.TimeUnit;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.StringRedisTemplate;
+import org.apache.commons.collections4.map.LRUMap;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,20 +21,36 @@ import org.springframework.stereotype.Service;
  * @since 2015年11月3日
  */
 @Service
-public class CacheService extends BasicService {
+public class CacheService extends BasicService implements InitializingBean, DisposableBean {
 
-    @Autowired
-    StringRedisTemplate redisTemplate;
-    
+    LRUMap<String, String> cache = new LRUMap<>(10000);
+
+    @Value("${logging.path}")
+    String filePath;
+
     public String get(String key) {
-        return redisTemplate.opsForValue().get(key);
+        return cache.get(key);
     }
-    
+
     public void set(String key, String value) {
-        redisTemplate.opsForValue().set(key, value);
+        cache.put(key, value);
     }
-    
-    public void set(String key, String value, long expire) {
-        redisTemplate.opsForValue().set(key, value, expire, TimeUnit.MILLISECONDS);
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(filePath + "/cache"));
+        LRUMap<String, String> temp = (LRUMap<String, String>) inputStream.readObject();
+        inputStream.close();
+        if (temp != null) {
+            cache = temp;
+        }
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(filePath + "/cache"));
+        outputStream.writeObject(cache);
+        outputStream.close();
     }
 }
